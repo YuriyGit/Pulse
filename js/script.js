0cd52de5ee36ee7187f26ef6f5c766840a0b040b
Yuriy <Yuriy.mayorov.web@mail.ru>
2021-05-11 10:25:51 +0400

    test

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ $(document).ready(function () {
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="/img/icons/left_solid.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="/img/icons/right_solid.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/icons/left_solid.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/icons/right_solid.svg"></button>',
         responsive: [
             {
                 breakpoint: 992,
